fcb15045dc23a7274a025a0f735876798e5d634c
yjkim <werbwerb@naver.com>
2020-08-26 13:50:50 +0900

    rxjava onjust() 테스트

--- src/main/java/com/basic/boot/api/controller/sample/RxJavaController2.java
+++ src/main/java/com/basic/boot/api/controller/sample/RxJavaController2.java
@@ -1,5 +1,7 @@
 package com.basic.boot.api.controller.sample;
 
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subscribers.DisposableSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +17,39 @@ public class RxJavaController2 {
     private static final Logger logger = LoggerFactory.getLogger(RxJavaController2.class);
 
     @GetMapping(value = "rxSync")
-    public String RxJavaSync() throws Exception {
+    public void RxJavaSync() throws Exception {
         logger.debug("RX_SYNC_STARTED " + Thread.currentThread().getName());
-        String threadName = Thread.currentThread().getName();
-
-        Random random = new Random();
-        int randomTime = random.nextInt(5000);
-        return threadName;
+        int i = 3;
+        Observable.just("hello", "yjkim", 5, 6, 7, 8, 9, 10)
+                .doOnNext(data -> new CustomSubscriber<>().onNext(data))
+                .doOnComplete(() -> new CustomSubscriber<>().onComplete())
+                .subscribe();
     }
 
     @GetMapping(value = "rxAsync")
     public void RxJavaAsync() {
         logger.debug("RX_ASYNC_STARTED");
+    }
+
+    public static class CustomSubscriber<T>  extends DisposableSubscriber<T> {
+        /**
+         * Logger.
+         */
+        private static final Logger logger = LoggerFactory.getLogger(CustomSubscriber.class);
+
+        @Override
+        public void onNext(T t) {
+            logger.debug("onNext() >>> " + t);
+        }
+
+        @Override
+        public void onComplete() {
+            logger.debug("onComplete() >>> ");
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            logger.debug("onError() >>> " + t);
+        }
     }
 }
