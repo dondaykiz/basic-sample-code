53f7dbf51b13667b3b82cc9f2bcd7b8bc97c928a
yjkim <werbwerb@naver.com>
2020-09-01 17:52:34 +0900

    rxjava main @junit 쪽 작업

---
+++ src/test/java/RxJavaTest.java
@@ -0,0 +1,69 @@
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RxJavaTest {
+
+    /**
+     * Logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(RxJavaTest.class);
+
+    @Test
+    public void rxJavaJust() {
+        logger.debug("RXJAVA_JUST_STARTED");
+        Observable.just("apple", "banana", "grape", "orange", "tomato")
+                .subscribe(logger::debug);
+    }
+
+    @Test
+    public void rxJavaArray() {
+        logger.debug("RXJAVA_ARRAY_STARTED");
+        String[] array = {"apple", "banana", "grape", "orange", "tomato"};
+        Observable.fromArray(array)
+                .subscribe(logger::debug);
+    }
+
+    @Test
+    public void rxJavaIterator() {
+        logger.debug("RXJAVA_ITERABLE_STARTED");
+        List<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("banana");
+        list.add("grape");
+        list.add("orange");
+        list.add("tomato");
+
+        Observable.fromIterable(list)
+                .subscribe(logger::debug);
+    }
+
+    @Test
+    public void rxJavaFlatMap() {
+        logger.debug("RXJAVA_FLATMAP_STARTED");
+        String[] array = {"apple", "banana", "grape", "orange", "tomato"};
+        Observable.fromArray(array)
+                .flatMap(data ->
+                        Observable.fromCallable(() -> formatString(data))
+                                .doOnNext(nextData -> logger.debug("DATA : {}, Thread: {}", nextData, Thread.currentThread().getName()))
+                                .subscribeOn(Schedulers.trampoline())
+                )
+                .subscribe();
+
+        logger.debug("RXJAVA_FLATMAP_FINISHED");
+    }
+
+    public String formatString(String data) {
+        try {
+            Thread.sleep(3000);
+            return data + " + @";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
